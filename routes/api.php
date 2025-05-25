669dd7a349f96d6bebfc4689ba7278f24f2d9729
benjamim <bhaskdelta2@gmail.com>
2025-05-25 18:27:42 -0300

    fix: replace ThrottleRequestsWithRedis middleware with ThrottleRequests

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\WeatherController;
-use Illuminate\Routing\Middleware\ThrottleRequestsWithRedis;
+use Illuminate\Routing\Middleware\ThrottleRequests;
 use Illuminate\Support\Facades\Route;
 
 Route::get('weather/{city}', [WeatherController::class, 'getCurrentWeather'])
-    ->middleware(ThrottleRequestsWithRedis::using('weather'))
+    ->middleware(ThrottleRequests::using('weather'))
     ->name('weather.get');
